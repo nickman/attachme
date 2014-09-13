59e4711250af5a0fefa484e5b2af8ad4e3c4890c
nickman <nwhitehead@heliosdev.org>
2014-09-13 07:21:39 -0400

    DevSynch

---
+++ src/main/java/com/heliosapm/attachme/jar/JarBuilder.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.heliosapm.attachme.jar;
+
+/**
+ * <p>Title: JarBuilder</p>
+ * <p>Description: </p>
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><b><code>com.heliosapm.attachme.jar.JarBuilder</code></b>
+ */
+
+public class JarBuilder {
+
+	/**
+	 * Creates a new 
+	 */
+	public JarBuilder() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
